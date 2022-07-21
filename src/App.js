ebfa8c04781df48215031b273b0475f9e49b0341
Hriday Gupta <hridaygupta100@hotmail.com>
2022-07-21 17:37:25 +0530

    adding werk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './components/Home.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="Home">
+        <button>Make word</button>
+        <button>Guess word</button>
+    </div>
+  );
+}
+
+export default Home;
