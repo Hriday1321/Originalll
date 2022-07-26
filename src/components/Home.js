79dc32c84a067b3f79094f5a6454681e06c1b11e
Hriday Gupta <hridaygupta100@hotmail.com>
2022-07-26 16:02:52 +0530

    tryiyng out stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,26 @@
+import React from "react"
+
 function Home() {
-  return (
-    <div className="Home">
-        <button>Make word</button>
-        <button>Guess word</button>
-    </div>
-  );
+  const [page, setPage] = React.useState('0');
+  if(page == '0') {
+    return (
+      <div className="Home">
+          <button onClick={setPage('1')}>Make word</button>
+          <button onClick={setPage('2')}>Guess word</button>
+      </div>
+    );}
+  else if(page == '1') {
+    return (
+      <div className="Home">
+        <button onClick={setPage('0')}>Go Back</button>
+      </div>
+    );}
+  else if(page == '2') {
+    return (
+      <div className="Home">
+        <button onClick={setPage('0')}>Go Back</button>
+      </div>
+    );}
 }
 
 export default Home;
