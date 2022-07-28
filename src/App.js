02ea733c662d47f1c55e91688419e2e876bfe886
Hriday Gupta <hridaygupta100@hotmail.com>
2022-07-28 22:43:39 +0530

    changing approach

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home.js'
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,10 @@
-import React from "react"
-
 function Home() {
-  const [page, setPage] = React.useState('0');
-  if(page == '0') {
     return (
       <div className="Home">
           <button >Make word</button>
           <button >Guess word</button>
       </div>
-    );}
-  else if(page == '1') {
-    return (
-      <div className="Home">
-        <button onClick={setPage('0')}>Go Back</button>
-      </div>
-    );}
-  else if(page == '2') {
-    return (
-      <div className="Home">
-        <button onClick={setPage('0')}>Go Back</button>
-      </div>
-    );}
+    )
 }
 
 export default Home;
