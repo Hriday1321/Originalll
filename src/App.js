1fc39aa806925037addf328aaca2ae84b3ea9dcd
Hriday Gupta <hridaygupta100@hotmail.com>
2022-07-29 14:48:24 +0530

    was using older syntax like a dumbo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './components/Home.js'
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/about" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+      </Routes>  
+      );
 }
 
 export default App;
