c7f2a84805ab0f9bf7cc1307f98338cb4294421b
Hriday Gupta <hridaygupta100@hotmail.com>
2022-08-01 22:15:19 +0530

    getting it all ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Home from './components/Home.js'
+import Home from './components/Home'
+import Make from './components/Make'
+import Guess from './components/Guess'
 
 function App() {
   return (
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/about" element={<Home />} />
+        <Route path="/make" element={<Make />} />
+        <Route path="/guess" element={<Guess />} />
       </Routes>  
       );
 }
