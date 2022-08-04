6d7ea902e9bf56034a1b198b57978d80746ff488
Hriday Gupta <hridaygupta100@hotmail.com>
2022-08-04 14:41:39 +0530

    make word functionality added

--- src/components/Make.js
+++ src/components/Make.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 export default function Make() {
+  const [word, makeWord] = React.useState('initial');
+
   return (
-    <div>Make</div>
+    <div className='make'>
+      <input type='text' placeholder='Enter Text' onChange={ (e) => {makeWord(e.target.value)}}></input>
+      {word}
+    </div>
   )
 }
