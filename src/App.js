1e748237da7487285de243e235c21cd24410767e
Hriday Gupta <hridaygupta100@hotmail.com>
2022-08-07 17:12:30 +0530

    not added any db as of yet but working lil by lil

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
 import Home from './components/Home'
 import Make from './components/Make'
 import Guess from './components/Guess'
 
 function App() {
+  const [words, setWords] = React.useState('first')
+  let props = {
+    words: words,
+    set: setWords
+  }
   return (
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/make" element={<Make />} />
+        <Route index element={<Home words = {words}/>} />
+        <Route path="/make" element={<Make {...props}/>} />
         <Route path="/guess" element={<Guess />} />
       </Routes>  
       );
