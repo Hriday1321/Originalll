a79e3c21aa0b0d58888c4297a6f61d59fbbd8ca8
Hriday Gupta <hridaygupta100@hotmail.com>
2022-08-14 16:15:06 +0530

    5 word limit bois

--- src/components/Make.js
+++ src/components/Make.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Nav from './Nav'
 export default function Make(props) {
   const [word, makeWord] = React.useState('initial');
-
+  const update = () => {
+    if(word.length === 5)
+        props.set(word)
+  }
   return (
     <div className='make'>
       <Nav />
       <div className='inputs'>
         <input type='text' placeholder='Enter Text' onChange={ (e) => {makeWord(e.target.value)}}></input>
-        <button onClick={() => {props.set(word)}}>Submit</button>
+        <button onClick={() => {update()}}>Submit</button>
       </div>
       {word} <br /> {props.words}
     </div>
