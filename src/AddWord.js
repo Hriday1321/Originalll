ce1314153d298004544c21e16616451923f16e9a
Hriday Gupta <hridaygupta100@hotmail.com>
2022-08-17 15:59:20 +0530

    component to add word to db

---
+++ src/AddWord.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {db} from './firebase'
+import {collection, addDoc, Timestamp} from 'firebase/firestore'
+
+function AddWord() {
+  return (
+    <div>AddWord</div>
+  )
+}
+
+export default AddWord
